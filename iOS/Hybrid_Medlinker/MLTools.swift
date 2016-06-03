f6aafbbc3f32e1f22b9c7c5f2f56070fb3a9c442
caiyang <caiyang@medlinker.com>
2016-06-03 11:04:25 +0800

    hasnavigation

--- iOS/Hybrid_Medlinker/MLTools.swift
+++ iOS/Hybrid_Medlinker/MLTools.swift
@@ -256,7 +256,7 @@ class MLTools: NSObject {
                     self.currentVC().presentViewController(navi, animated: true, completion: nil)
                 }
                 else {
-                    if let animate =  args["navigation"] as? String where animate == "none" {
+                    if let hidden =  args["hasnavigation"] as? Bool where hidden == false {
                         self.currentNavi().navigationBarHidden = true
                     }
                     else {
