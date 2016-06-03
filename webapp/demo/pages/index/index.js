30b8de3fad1122893264b329e1ff99c50dc26cb6
caiyang <caiyang@medlinker.com>
2016-06-03 12:59:27 +0800

    demo 17

--- webapp/demo/pages/index/index.js
+++ webapp/demo/pages/index/index.js
@@ -18,7 +18,7 @@
             this.template = layoutHtml;
             this.commonstyle = commonStyle;
             this.style = indexStyle;
-alert(222)
+alert(333)
             this.events = {
                 'click .js-items li': function (e) {
                     var el = $(e.currentTarget);
