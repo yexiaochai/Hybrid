bb477b33e10e9ffd5b436737c0d7f1a14d321dff
caiyang <caiyang@medlinker.com>
2016-07-11 16:22:43 +0800

    拆分出SDK代码

---
+++ iOS/DogHybrid/DogHybrid/DogHybrid.h
@@ -0,0 +1,39 @@
+//
+//  DogHybrid.h
+//  DogHybrid
+//
+//  Created by caiyang on 16/7/7.
+//  Copyright © 2016年 Cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DogHybrid.
+FOUNDATION_EXPORT double DogHybridVersionNumber;
+
+//! Project version string for DogHybrid.
+FOUNDATION_EXPORT const unsigned char DogHybridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DogHybrid/PublicHeader.h>
+//#import <DogHybrid/AFNetworking.h>
+//#import <DogHybrid/UIKit+AFNetworking.h>
+
+
+#import <DogHybrid/UIControl+YYAdd.h>
+#import <DogHybrid/YYModel.h>
+
+
+//#import "UIKit+AFNetworking.h"
+//#import "YYModel.h"
+//
+#import <JavaScriptCore/JavaScriptCore.h>
+//#import <objc/runtime.h>
+//
+//#import "ZipArchive.h"
+#import <DogHybrid/ZipArchive.h>
+
+//#import
+//#import "MLHybridTools.swift"
+//#import <DogHybrid/DogHybirdURLProtocol.swift>
+
+
---
+++ iOS/HybridApp/HybridApp/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  HybridApp
+//
+//  Created by caiyang on 16/7/7.
+//  Copyright © 2016年 Cy. All rights reserved.
+//
+
+import UIKit
+import DogHybrid
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        NSURLProtocol.registerClass(DogHybridURLProtocol)
+        return true
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        MLHybridTools().analysisUrl(url.absoluteString)
+        return true
+    }
+
+}
+
