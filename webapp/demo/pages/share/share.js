ae653e7dd7f4884d924e1fdd0efd15be0af8e2c5
wanglei <wanglei@medlinker.com>
2017-02-06 16:44:40 +0800

    demo

--- webapp/demo/pages/share/share.js
+++ webapp/demo/pages/share/share.js
@@ -17,36 +17,168 @@
             this.events = {
                 'click .js-btn01': function () {
 
-/*打开native的分享框
-如果没有传递参数则
-title&desc直接取title标签的title
-image取第一张图片的链接,没有就为空
-link取网页的url
-native图标包括:
-1 发送给微信朋友
-2 分享到朋友圈
-3 发送给qq好友
-4 分享到qq空间
-5 分享到新浪微博
-6 复制链接
-*/
-_.requestHybrid({
-    tagname: 'openNativeUI',
-    param: {
-        title: '分享标题',
-        desc: '分享描述',
-        image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
-        link: 'http://medlinker.com'
-    },
-    callback: function(data) {
-       //分享成功时候的回调
+                    /*打开native的分享框
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     1 发送给微信朋友
+                     2 分享到朋友圈
+                     3 发送给qq好友
+                     4 分享到qq空间
+                     5 分享到新浪微博
+                     6 复制链接
+                     */
+                    _.requestHybrid({
+                        tagname: 'openNativeUI',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+alert('分享成功');
+                        }
+                    });
 
-    }
-});
+                },
+                'click .js-btn02': function () {
+
+                    /*
+                     发送给微信好友
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     */
+                    _.requestHybrid({
+                        tagname: 'shareAppMessage',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+alert('分享成功');
+                        }
+                    });
+
+                },
+                'click .js-btn03': function () {
+
+                    /*
+                     发送给qq好友
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     */
+                    _.requestHybrid({
+                        tagname: 'shareTimeline',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+alert('分享成功');
+                        }
+                    });
+
+                },
+                'click .js-btn04': function () {
+
+                    /*
+                     分享到微信朋友圈
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     */
+                    _.requestHybrid({
+                        tagname: 'shareQQ',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+alert('分享成功');
+                        }
+                    });
+
+                },
+            'click .js-btn05': function () {
+
+                    /*
+                     分享到微信朋友圈
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     */
+                    _.requestHybrid({
+                        tagname: 'shareQZone',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+                            alert('分享成功');
+                        }
+                    });
+
+                },
+                'click .js-btn06': function () {
+
+                    /*
+                     分享到新浪
+                     如果没有传递参数则
+                     title&desc直接取title标签的title
+                     image取第一张图片的链接,没有就为空
+                     link取网页的url
+                     native图标包括:
+                     */
+                    _.requestHybrid({
+                        tagname: 'shareSina',
+                        param: {
+                            title: '分享标题',
+                            desc: '分享描述',
+                            image: 'http://medlinker.com/h5/interlocution/static/img/icon-camera.png',
+                            link: 'http://medlinker.com'
+                        },
+                        callback: function(data) {
+                            //分享成功时候的回调
+                            alert('分享成功');
+                        }
+                    });
+
+                },
+                'click .js-btn07': function () {
+
+                    //复制当前网页链接
+                    _.requestHybrid({
+                        tagname: 'copyLink'
+                    });
 
                 }
-
-            }
+            };
 
         },
 
