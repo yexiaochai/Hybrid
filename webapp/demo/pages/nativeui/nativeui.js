6aadc4dc456f1843eacf6b7ceff98e5fb426053f
wanglei <wanglei@medlinker.com>
2017-02-07 17:47:24 +0800

    demo

--- webapp/demo/pages/nativeui/nativeui.js
+++ webapp/demo/pages/nativeui/nativeui.js
@@ -32,6 +32,25 @@
 _.requestHybrid({
     tagname: 'showKeyboard',
     param: {
+
+        /*===图片参数===*/
+        //如果该键盘带参数的话一定要传以下参数
+        hasImg: 1, //必须传hasImg为1,否则认为键盘没有图片
+
+        //文件所属分类,包括（transform(出转诊),casem(病例), question(问题), post(帖子), help(求助), secret(深夜病房), logo, avatar(头像)，chat(聊天), idCard(身份证), profile(用户信息),panel, spread, media,live)
+        bucket: 'transform',
+        //1代表文件传入公共盘，所有人可以访问。0代表私人盘，需要授权访问
+        isPublic: 1,
+        //水印文字 ,如果没传参数则没有水印
+        waterText: '水印文字',
+
+        //以下是基本参数
+        //1-9张限制
+        count: 1,
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        /*===图片参数===*/
+
         /*键盘按钮文案
          Done      —>    完成
          Send      —>    发送
