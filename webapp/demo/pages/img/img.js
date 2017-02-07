d5ab7eefbdca4519cadfb269c1c789b21df3f3d9
wanglei <wanglei@medlinker.com>
2017-02-07 11:53:19 +0800

    demo

---
+++ webapp/demo/pages/img/img.js
@@ -0,0 +1,59 @@
+﻿define([
+    'BaseView',
+    'text!PagePath/img/tpl.layout.html',
+    'text!StylePath/common.css'
+], function (AbstractView,
+             layoutHtml,
+             commonStyle) {
+
+    return _.inherit(AbstractView, {
+        propertys: function ($super) {
+
+            $super();
+            var scope = this;
+
+            this.template = layoutHtml;
+            this.commonstyle = commonStyle;
+
+            this.events = {
+                'click .js-btn01': function () {
+
+                    /*
+                    关闭当前页面,回到native上一步操作
+                     */
+                    _.requestHybrid({
+                        tagname: 'closeWindow'
+                    });
+
+                }
+            };
+
+        },
+
+        initHeader: function () {
+            var opts = {
+                view: this,
+                title: '图片 Demo',
+                back: function () {
+                    this.back();
+                }
+            };
+            this.header.set(opts);
+        },
+
+        initElement: function () {
+
+        },
+
+        addEvent: function ($super) {
+            $super();
+
+            this.on('onShow', function () {
+
+
+            });
+        }
+
+    });
+
+});
